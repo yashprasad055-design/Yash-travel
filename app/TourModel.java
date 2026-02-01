1b53071379d8421c253719ecb42bed15ca2ae09b
yashprasad055-design <yashprasad055@gmail.com>
2026-02-01 22:26:57 +0530

    Add TourModel class for tour details

---
+++ app/TourModel.java
@@ -0,0 +1,11 @@
+package com.yashtourtravel.app;
+
+public class TourModel {
+    String name, price, desc;
+
+    public TourModel() {}
+
+    public String getName() { return name; }
+    public String getPrice() { return price; }
+    public String getDesc() { return desc; }
+}
